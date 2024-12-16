d47be7c3b0e0cfcd6928efad06495124da2450dc
Kaushalyasl <149307067+Kaushalyasl@users.noreply.github.com>
2024-12-16 18:24:59 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94750122621"
+const SESSION_ID = "PRABATH-MD~eV0GVRYY#jzHnjT7q3h1IMY_Xzd32qd0ydkNRqh7ejSFZk_Tr3X4"
 
 
 module.exports = {
